d35fb839dfaaf79ffdacd94429baee508e7d4dd9
Arul Selvan <aselvan@selvans.net>
2025-01-18 17:32:53 -0600

    Updated caveats to include home variable

--- Formula/aselvan-scripts.rb
+++ Formula/aselvan-scripts.rb
@@ -1,11 +1,9 @@
 class AselvanScripts < Formula
-  desc "aselvan.scripts --- a random collection of utility scripts"
+  desc "aselvan-scripts --- a collection of handy utility scripts for macOS & Linux"
   homepage "https://github.com/aselvan/scripts"
-  
   version "v25.01.18"
   url "https://github.com/aselvan/scripts/archive/refs/tags/v25.01.18.tar.gz"
   sha256 "e10f4c0fc40a6c33bbf3af5f60b5033d1681fbab84be39a26dfa1a2005a35348"
-
   license "MIT"
 
   def install
@@ -15,14 +13,15 @@ class AselvanScripts < Formula
     bin.install_symlink prefix => name
   end
 
-
   caveats do
     <<~EOS
       To add #{name} to your PATH, add the following line to your ~/.bashrc:
-        export PATH="$PATH:#{opt_bin}/utils:#{opt_bin}/security:#{opt_bin}/tools:#{opt_bin}/macos:#{opt_bin}/firewal"
+        export GITHUB_SCRIPTS_HOME="#{opt_bin}"
+        export PATH="$PATH:#{opt_bin}/utils:#{opt_bin}/security:#{opt_bin}/tools:#{opt_bin}/macos:#{opt_bin}/firewall"
 
       Or, if you prefer, you can use the following command:
-        echo 'export PATH="$PATH:#{opt_bin}/utils:#{opt_bin}/security:#{opt_bin}/tools:#{opt_bin}/macos:#{opt_bin}/firewal"' >> ~/.bashrc
+        echo 'export GITHUB_SCRIPTS_HOME="#{opt_bin}"' >> ~/.bashrc
+        echo 'export PATH="$PATH:#{opt_bin}/utils:#{opt_bin}/security:#{opt_bin}/tools:#{opt_bin}/macos:#{opt_bin}/firewall"' >> ~/.bashrc
     EOS
   end
 
