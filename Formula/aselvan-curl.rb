4f795a07bbd5599eb4d9b4a1483c6c0a89a0b16b
Arul Selvan <aselvan@selvans.net>
2025-01-19 09:14:54 -0600

    updated curl name

--- Formula/aselvan-curl.rb
+++ Formula/aselvan-curl.rb
@@ -1,4 +1,4 @@
-class Curl < Formula
+class AselvanCurl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.haxx.se/"
   url "https://curl.haxx.se/download/curl-7.64.0.tar.bz2"
