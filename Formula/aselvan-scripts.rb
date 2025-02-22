af25d8a8c4a03fb2245bfff4655e23244a91f659
Arul Selvan <aselvan@selvans.net>
2025-02-22 14:40:36 -0600

    Latest updates

--- Formula/aselvan-scripts.rb
+++ Formula/aselvan-scripts.rb
@@ -5,7 +5,7 @@ class AselvanScripts < Formula
   ### all 3 of these varibables below should be updated on new release
   version "v25.02.21"
   url "https://github.com/aselvan/scripts/archive/refs/tags/v25.02.21.tar.gz"
-  sha256 "bc368ba04fec7ee0c87fc598a4cc5db022ffee4c229d2bc608f6ef3f4d548835"
+  sha256 "58e7fd0acb0f5811e1d2faf971b66bbf55b35efb2b6be947a8c62cec1e7f07ca"
   
   # licence release
   license "MIT"
